6954b9cb88eea66c2cec7a1163fbbc60e86c496c
damibran <olegevdk@gmail.com>
2024-02-29 21:22:32 +0300

    bug fix

--- Source/Components/СoncreteComponent/GameToBallBridge.h
+++ Source/Components/СoncreteComponent/GameToBallBridge.h
@@ -2,8 +2,9 @@
 
 namespace dmbrn
 {
+	// actually this could be class extendor of game to component bridge
 	struct GameToBallBridge
 	{
-		int lRacketScore, rRacketScore;
+		int &lRacketScore, &rRacketScore;
 	};
 }
